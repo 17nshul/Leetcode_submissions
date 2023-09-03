afdd43d58ec592bf9ea562608a5a2924b217a5a1
Anshul Sharma <76740420+17nshul@users.noreply.github.com>
2023-09-03 16:31:39 +0530

    Time: 7 ms (100.00%) | Memory: 5.9 MB (80.00%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.cpp
+++ 2998-count-symmetric-integers/count-symmetric-integers.cpp
@@ -1,19 +1,29 @@
-class Solution {public:
-        int countSymmetricIntegers(int low, int high) {
-        int ans = 0;
-        for (int i = low; i <= high; ++i)
-            ans += isSymmetricInteger(i);
-        return ans;
+namespace {
+constexpr bool isSymmetricInteger(int x) {
+    if (x >= 10 && x <= 99) {
+        return x / 10 == x % 10;
+    } else if (x >= 1000 && x <= 9999) {
+        const int lo = x % 100;
+        const int hi = x / 100;
+        return lo % 10 + lo / 10 == hi % 10 + hi / 10;
     }
+    return false;
+}
+    
+constexpr auto gen_ps() {
+    array<int, 10001> ans = {};
+    for (int i = 1; i < size(ans); ++i)
+        ans[i] = ans[i - 1] + isSymmetricInteger(i);
+    return ans;
+}
+    
+constexpr auto ps = gen_ps();
+    
+}  // namespace
 
-    bool isSymmetricInteger(int x) {
-        if (x >= 10 && x <= 99) {
-            return x / 10 == x % 10;
-        } else if (x >= 1000 && x <= 9999) {
-            const int lo = x % 100;
-            const int hi = x / 100;
-            return lo % 10 + lo / 10 == hi % 10 + hi / 10;
-        }
-        return false;
+class Solution {
+public:
+    static int countSymmetricIntegers(int low, int high) {
+        return ps[high] - ps[low - 1];
     }
 };
