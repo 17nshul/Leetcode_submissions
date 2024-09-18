ac613809b9bcb2d4865f872330cd6c04c6854b84
Anshul Sharma <76740420+17nshul@users.noreply.github.com>
2024-09-18 23:54:09 +0530

    Time: 43 ms (98.65%), Space: 85.8 MB (9.07%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -4,21 +4,21 @@ public:
         //bitwise and of two number will always be less than max of those two
         //subarray where only the max number is present
         //sliding window that keeps the subarray of max elements
-        ios::sync_with_stdio(0);
+ios::sync_with_stdio(0);
 cin.tie(0);
 cout.tie(0);
         int n = nums.size();
         int mw=0;
         int currw = 0;
         int mx = INT_MIN;
-        for(int i = 0; i < n; i++){
-            if(nums[i] > mx) {
-                mx = nums[i];
-            }
-        }
+        
         for(int i=0;i<n;i++){
             if(nums[i]==mx){
                 currw++;
+            }else if(nums[i]>mx){
+                mx=nums[i];
+                currw=1;
+                mw=1;
             }else{
                 mw = max(mw,currw);
                 currw=0;
