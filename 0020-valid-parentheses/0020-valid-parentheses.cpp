c52604ae489b245296b617e5360d7c4fb132109f
Anshul Sharma <76740420+17nshul@users.noreply.github.com>
2025-03-15 20:28:36 +0530

    Time: 0 ms (100%), Space: 8.7 MB (83.9%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     bool isValid(string s) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
+        //use stack and a map
         map<char,char> m;
-        m['(']=')';
-        m['[']=']';
-        m['{']='}';
+        m.insert({'[',']'});
+        m.insert({'(',')'});
+        m.insert({'{','}'});
+
         stack<char>st;
-        for(auto c:s){
+
+        for(auto &c: s){
             if(m.find(c)!=m.end()){
-                //mil gaya in map
-                st.push(c);   
-            }else{
-                if(st.empty() || m[st.top()] != c){
+                st.push(c);
+            }
+            else {
+                if(st.empty()||m[st.top()]!=c){
                     return false;
-                }
-                else{
-                    st.pop();
-                }
+                }      
+                st.pop();
             }
         }
         return st.empty();
